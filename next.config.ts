1e8aa0e63a8fd36b5efefc67f428e61de2404a00
osvaldo hinojosa <osvaldo.javier14@gmail.com>
2025-07-04 06:14:01 -0600

    feat: update image pathname pattern for remote images in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'randomuser.me',
         port: '',
-        pathname: '/api/portraits/thumb/**',
+        pathname: '**',
       },
       {
         protocol: 'https',
